264ba87f6f507a6655e424b3bc002a813ee6858a
alexis <alexisatindehou18@gmail.com>
2026-03-07 18:48:44 +0000

    Ajout des fichiers de base : Makefile, README.md, contact.h et main.c

---
+++ include/contact.h
@@ -0,0 +1,12 @@
+#ifndef CONTACT_H
+#define CONTACT_H
+
+typedef struct {
+    char nom[50];
+    char prenom[50];
+    char telephone[20];
+    char email[50];
+    char adresse[100];
+} Contact;
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "../include/contact.h"
+
+int main() {
+    printf("Répertoire de contacts\n");
+    return 0;
+}
