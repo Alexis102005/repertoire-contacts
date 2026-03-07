bbce715d84060d9641f60fffacc6f342df153738
alexis <alexisatindehou18@gmail.com>
2026-03-07 19:29:21 +0000

    creaion du rep_contact

--- src/main.c
+++ src/main.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
 #include "../include/contact.h"
-
 int main() {
-    printf("Répertoire de contacts\n");
+    Contact c;
+
+    printf("=== Répertoire de Contacts ===\n\n");
+
+    printf("Nom : ");
+    scanf("%49s", c.nom);
+
+    printf("Prénom : ");
+    scanf("%49s", c.prenom);
+
+    printf("Téléphone : ");
+    scanf("%19s", c.telephone);
+
+    printf("Email : ");
+    scanf("%49s", c.email);
+
+    printf("\n--- Contact enregistré ---\n");
+    printf("Nom     : %s %s\n", c.prenom, c.nom);
+    printf("Tel     : %s\n", c.telephone);
+    printf("Email   : %s\n", c.email);
+
     return 0;
 }
